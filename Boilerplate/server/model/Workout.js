0958f4f2997f2be8a9371f9dfac924217049f5f5
dci-student <roxanamaria.cuntan@dci.education>
2023-10-05 10:08:53 +0200

    newchanges

--- Boilerplate/server/model/Workout.js
+++ Boilerplate/server/model/Workout.js
@@ -1,33 +1,32 @@
-
 import mongoose from "mongoose";
 
-const WorkoutSchema = new mongoose.Schema({
-    gender: {
-      type: String,
-      enum: ['Male', 'Female']
-    },
-    characteristics: {
-      Male: characteristicsSchema,
-      Female: characteristicsSchema
-    }
-  });
-  
-  
-  const characteristicsSchema = new mongoose.Schema({
+// Define characteristicsSchema first
+const characteristicsSchema = new mongoose.Schema({
   weight: Number,
   height: Number,
   muscleType: {
     type: String,
-    enum: ['Endomorphic', 'Mesomorphic', 'Ectomorphic']
+    enum: ["Endomorph", "Mesomorph", "Ectomorph"],
   },
   dietHabits: {
     type: String,
-    enum: ['Omnivore', 'Vegetarian', 'Vegan']
+    enum: ["Omnivore", "Vegetarian", "Vegan"],
   },
-  sleepingHabits: String,
-  enum: ['normal', 'midday break', 'irregular']
+  sleepingHabits: {
+    type: String,
+    enum: ["normal", "midday break", "irregular"],
+  },
 });
 
-const Workout = mongoose.model('Workout', WorkoutSchema);
+// Define WorkoutSchema using characteristicsSchema
+const WorkoutSchema = new mongoose.Schema({
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+  },
+  characteristics: characteristicsSchema,
+});
 
-module.exports = Workout;
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+export default Workout;
