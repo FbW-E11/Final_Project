6f144a1a42fee48f0dada53523926e93b28b2afc
Adjei Solomon <118280093+HubSolomon@users.noreply.github.com>
2023-10-10 16:11:17 +0200

    updated-solomon

--- Boilerplate/server/app.js
+++ Boilerplate/server/app.js
@@ -7,7 +7,9 @@ import workoutRoute from "./routes/workoutRoute.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:3000",credentials:true
+}));
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/workout", workoutRoute);
@@ -18,13 +20,12 @@ mongoose
   .connect(
     "mongodb+srv://Final-Pj-DB:XIG7hEPj5KDpGELN@cluster0.meb2lvg.mongodb.net/FutureConnect-DB"
   )
-
   .then(() => {
     console.log("connected");
   })
   .catch((err) => {
     console.log("error", err);
   });
-app.listen(5002, (req, res) => {
-  console.log("listening on port 5002");
+app.listen(5001, (req, res) => {
+  console.log("listening on port 5001");
 });
