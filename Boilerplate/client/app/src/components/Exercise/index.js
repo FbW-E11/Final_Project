0bc614b049a82d4c60f02c3e16462d1772aaf57a
RodrigueTJANI <rodriguegaby.tjanipie@dci.education>
2023-10-11 16:02:45 +0200

    Update 1110

---
+++ Boilerplate/client/app/src/components/Exercise/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../Exercise/style.css";
+
+
+const Exercise = () => {
+  return (
+    <div>
+        <h1>EXERCISE PAGE</h1>
+    </div>
+  )
+}
+
+export default Exercise
